fb0ac5fe785037eea4a8f70337dce8cd9919cef2
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2025-03-05 18:38:51 -0800

    Update settings.py

--- medi/settings.py
+++ medi/settings.py
@@ -20,7 +20,7 @@
 ***REMOVED***
 
 ***REMOVED***
-SECRET_KEY = 'e!w0df=!%4gp$p03_^bxpu)%4bhd6w5w#cm((p(nqqt9aei%nj'
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
@@ -131,7 +131,7 @@ SECRET_KEY = 'e!w0df=!%4gp$p03_^bxpu)%4bhd6w5w#cm((p(nqqt9aei%nj'
 ***REMOVED***
 
 
-SEND_GRID_API_KEY = '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'
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
